8ee6a45c61f279fb4b1787f454630938a4fce923
Sushant Lenka <frostbitecryengine300@gmail.com>
2019-06-10 18:57:43 +0530

    added logout, login and verification

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@ const GoogleStrategy=require('passport-google-oauth20').Strategy;
 const mongoose=require('mongoose');
 const keys=require('./keys');
 
+const User=mongoose.model('users')
+
 module.exports=function(passport){
     passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -9,6 +11,39 @@ module.exports=function(passport){
         callbackURL: "/auth/google/callback",
         proxy:true
     },(accessToken, refreshToken, profile, done)=>{
+        //const image=profile.photos[0].value.substring(0,profile.photos[0].value.indexof('?'));
+        const image = profile.photos[0].value.replace('s50/', '');
+        const newUser={
+            googleID:profile.id,
+            firstName:profile.name.givenName,
+            lastName:profile.name.familyName,
+            email:profile.emails[0].value,
+            image:image
+        }
 
-    }))
+        User.findOne({
+            googleID:profile.id
+        }).then(user=>{
+            if(user){
+                done(null,user);
+            }
+            else{
+                new User(newUser)
+                .save()
+                .then(user => done(null,user));
+            }
+        });
+
+
+
+    }));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
 }
