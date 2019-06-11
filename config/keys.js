2870f0fa4eb7484d86fb608f5dc932a550ec3b8a
Sushant Lenka <frostbitecryengine300@gmail.com>
2019-06-11 20:24:39 +0530

    changes for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports={
-    mongoURI:'mongodb+srv://sush:sush@storiescluster-cowdq.mongodb.net/storybooks-dev',
-    googleClientID:'1030041767381-4t13pil2vvl03get36h08sk00pvg7l0t.apps.googleusercontent.com',
-    googleClientSecret:'m-ghsIdV7gLaETx3SDgxl2yT'
+if(process.env.NODE_ENV==='production'){
+    module.exports=require('./keys_prod');
+}else{
+module.exports=require('./keys_dev');
 }
